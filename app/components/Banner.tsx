dee97ac25f0131389e0447341d68ffd8ce4183ec
tonylin6032 <103610353+tonylin6032@users.noreply.github.com>
2026-01-09 15:51:38 -0800

    work please

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -7,7 +7,7 @@ import logo from "@/public/sageroboticslogo.png";
 export default function Banner() {
   return (
     <div className="w-full bg-black">
-      <div className="mx-auto flex h-16 max-w-screen-2xl items-center px-4 sm:px-6">
+      <div className="mx-auto flex h-20 max-w-screen-2xl items-center px-6">
         <Link
           href="/"
           className="flex w-full items-center justify-center sm:justify-start gap-3"
@@ -15,12 +15,12 @@ export default function Banner() {
           <Image
             src={logo}
             alt="Sage Robotics Logo"
-            width={44}
-            height={44}
-            className="h-11 w-11 sm:h-12 sm:w-12 object-contain"
+            width={60}
+            height={60}
+            className="h-[60px] w-[60px] object-contain"
             priority
           />
-          <h1 className="text-white font-semibold leading-none text-xl sm:text-3xl md:text-4xl">
+          <h1 className="text-5xl font-semibold text-white leading-none">
             Sage Hill Robotics
           </h1>
         </Link>
